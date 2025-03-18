a9754c298fa30b248af6d9cbeb4da510b5219663
yuri134x <yurifilipe2004@gmail.com>
2025-03-18 12:30:12 -0300

    criação da api (DIO)

---
+++ projeto-api/src/test/java/me/dio/projeto_api/ProjetoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.projeto_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
